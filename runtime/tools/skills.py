b735fde7a0a0381b2d41a9083191e4efb46c2b31
Surya <lifesuryamanagement@gmail.com>
2026-06-18 00:24:12 +0700

    Add skills runtime compatibility module

---
+++ runtime/tools/skills.py
@@ -0,0 +1,11 @@
+from src.tool_implementations import (
+    _parse_tool_args,
+    _skill_dump,
+    do_manage_skills,
+)
+
+__all__ = [
+    "_parse_tool_args",
+    "_skill_dump",
+    "do_manage_skills",
+]
