dd139531844cf8b34773a4a1804a6e46561a4972
Surya <lifesuryamanagement@gmail.com>
2026-06-19 19:34:17 +0700

    Add runtime vault compatibility module

---
+++ runtime/tools/vault.py
@@ -0,0 +1,11 @@
+from src.tool_implementations import (
+    do_vault_search,
+    do_vault_get,
+    do_vault_unlock,
+)
+
+__all__ = [
+    "do_vault_search",
+    "do_vault_get",
+    "do_vault_unlock",
+]
